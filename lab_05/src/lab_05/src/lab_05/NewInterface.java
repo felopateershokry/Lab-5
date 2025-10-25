752a9ba8aa9e0570764e2b5a0aa09b2730e611ac
ahmed-salama-R <ahmedibrahimss01230@gmail.com>
2025-10-25 21:33:57 +0300

    GUI Front

---
+++ lab_05/src/lab_05/src/lab_05/NewInterface.java
@@ -0,0 +1,16 @@
+package lab_05;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+
+
+/**
+ *
+ * @author GAMING
+ */
+public interface NewInterface {
+    public String lineRepresentation () ; 
+    public int getsearchId () ; 
+}
